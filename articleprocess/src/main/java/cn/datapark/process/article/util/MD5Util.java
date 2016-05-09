afb84c4bed3ec1e76125157346b5c0aaba51de65
Qiu Wenyuan <qiuwenyuan@datapark.cn>
2016-05-09 11:31:43 +0800

    MD5工具类

---
+++ articleprocess/src/main/java/cn/datapark/process/article/util/MD5Util.java
@@ -0,0 +1,29 @@
+package cn.datapark.process.article.util;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Util {
+	public static String getMD5Str(String str) {
+		 String re_md5 = new String();
+	        try {
+	            MessageDigest md = MessageDigest.getInstance("MD5");
+	            md.update(str.getBytes());
+	            byte b[] = md.digest();
+	            int i;
+	            StringBuffer buf = new StringBuffer("");
+	            for (int offset = 0; offset < b.length; offset++) {
+	                i = b[offset];
+	                if (i < 0)
+	                    i += 256;
+	                if (i < 16)
+	                    buf.append("0");
+	                buf.append(Integer.toHexString(i));
+	            }
+	 
+	            re_md5 = buf.toString();
+	        } catch (NoSuchAlgorithmException e) {
+	            e.printStackTrace();
+	        }
+	        return re_md5;
+	}
+}
